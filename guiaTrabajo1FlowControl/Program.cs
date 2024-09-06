f1c5da2ecfdacfada3fc65b7cee5d51710a77a80
Andrés Castillo <165836520+acr301@users.noreply.github.com>
2024-09-06 14:12:54 -0600

    iniciando tarea 30pts

---
+++ guiaTrabajo1FlowControl/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace guiaTrabajo1FlowControl;
+
+
+public class Par
+{
+  public int numero;
+  public int dos = 2;
+  public bool esParONo;
+
+  public bool VerificarEsParoONo(int numero, int dos)
+  {
+
+    if (numero mod dos = 0)
+    {
+      esParONo = true;
+    }
+    else
+    {
+      esParONo = false;
+    }
+
+    return esParONo;
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Par cinco = new Par();
+    Par.modular(5, 2);
+
+  }
+}
