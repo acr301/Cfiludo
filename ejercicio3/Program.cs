497330ba1ff8bdae2ae9b090698ef5c0354d36a4
Andres Castillo <dalvarezp@uamv.edu.ni>
2024-09-17 07:18:11 -0600

    individualizando tarea

--- ejercicio3/Program.cs
+++ ejercicio3/Program.cs
@@ -8,49 +8,118 @@
 
 public class Empresa
 {
-  public class Rangos
-  {
-
-    // TO-DO -> hacer los arrays dinamicos
-    // en cuanto que se les puedan pasar estos rangos
-    int rangoVendedores;
-
-    int rangoVentasDia;
-    int rangoVolantesDia;
-
-    int rangoProductos;
-
-    int cantidadDiasMes;
-    int rangoVolantesMensual;
-
-  }
-
   public class Venta
   {
-    //4 vendedores
+    //4 vendedorespublicstring
     static string[] vendedores = new string[4] { "Andres", "Emilio", "Gabriel", "Fatima" };
 
 
     //productos de 1 a 5
     static string[] productos = new string[5] { "Gaseosa", "Chicle", "Chocolate", "Encendedor", "Recarga" };
 
-    //volante contiene 
-    int[][] volante = new int[][] { vendedores, productos };
+    //total de productos por dolares por dia
+    //5x5 donde el nombre del producto va con su respectivo total diario
+    //totalProductosDol[i, 0 ] = numnombreproducto, totalProductosDol[0, j] = totalproductodiariodol
+    static int[,] totalProductosDol = new int[5, 5];
+
+    //ventas de supuestos volantes del mes anterior
+    //ventas totales por vendedor y producto
+
+    //vendedor [i,] producto [,j]
+    int[,] ventas = new int[4, 5];
+
+    //totalizados[0, ] es por producto
+    //totalizados[ ,0] es por vendedor
+    int[,] totalizados = new int[1, 1];
+
+    public class Rangos
+    {
+      // TO-DO -> mejorar encapsulacion, usar propiedades para
+      // rangos y DRY (do not repeat yourself)
+      public int rangoVendedores = vendedores.Length;
+      int rangoVentasDia;
+      int rangoVolantesDia;
+
+      public int rangoProductos = productos.Length;
+
+      int cantidadDiasMes;
+    }
   }
   public class Volante
   {
-    public void VolanteDiario()
+    public void Diario()
     {
 
+      //cada vendedor entrega por dia entre 0 y 5 volantes
+      //cada volante contiene: num vendedor, num producto,
+      //valor total en dolares del producto vendido ese dia
+      int[,,] volante = new int[4, 5, 5];
     }
-    public void VolanteMensual()
+
+    /* ----------------------------ASUMIREMOS QUE LO DE 1 DIA ES LO DE TODO EL MES--------------
+     * es decir, volante[,,] = volanteMensual[,,]
+     * donde los fors nesteados para lo diario fueron repetidos
+     * x cantidad de dias del mes y acumulados en su 
+     * totalProductosDolMensual respectivo
+    public void Mensual()
     {
       //TO-DO -> que averigue verdaderamente cuantos dias tuvo el mes anterior
-      //renglones de producto1 a producto5 y ventasTotalesXvendedorXmes
-      //omitiendo venta0, columnas de venta1 a venta4 y ventasTotalesXproductoXmes
-      int[,] volanteMensual = new int[6, 5];
+      //int[,] volanteMensual = new int[6, 5];
     }
+    */
   }
+
+  public class Append
+  {
+    public void CrearVolanteDiario(int[,,] volante, string[] vendedores, string[] productos, int[,] totalProductosDol)
+    {
+      int acumuladorTotalProductosDol;
+
+      // append todos los vendedores al indice correspondiente
+      for (int i = 0; i < vendedores.Length; i++)
+      {
+        volante[i, 0, 0] = Array.IndexOf(vendedores, i);
+
+        // append todos los productos al indice correspondiente
+        for (int j = 0; j < productos.Length; j++)
+        {
+          totalProductosDol[j, 0] = Array.IndexOf(productos, j);
+          volante[0, j, 0] = Array.IndexOf(productos, j);
+
+          //va sumandolos a un gran total
+          acumuladorTotalProductosDol = +totalProductosDol[0, j];
+
+          //este loop era innecesario
+          for (int k = 0; k < totalProductosDol.Length; k++)
+          {
+            volante[0, 0, k] = acumuladorTotalProductosDol;
+          }
+        }
+      }
+    }
+
+    // ventas es 4x5
+    // volante es 4x5x5
+    // totalProductosDol es 5x5
+    public void CrearVentas(int[,] totalProductosDol, int[,] ventas, int rangoProductos, int rangoVendedores, int[,,] volante, int[,] totalizados)
+    {
+      int acumuladorTotalizadoProducto;
+      int acumuladorTotalizadoVendedor;
+
+      for (int i = 0; i < rangoVendedores; i++)
+      {
+        ventas[i, 0] = totalProductosDol
+        acumuladorTotalizadoVendedor = +ventas[i, 0];
+
+        for (int j = 0; j < rangoProductos; j++)
+        {
+          ventas[0, j] =
+        }
+      }
+    }
+
+  }
+
 
 
 }
