e2c8f8392fbd3500d8551b6085cdabd52a94682f
acr301 <acrothschuh@uamv.edu.ni>
2024-10-14 07:50:45 -0600

    iniciando el ejercicio 2 grupal

---
+++ sesion141024/numero2Grupal/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace numero2Grupal;
+
+class Program
+{
+
+    public class Inventario
+    {
+
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
