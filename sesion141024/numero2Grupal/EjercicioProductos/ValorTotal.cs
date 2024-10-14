82ae278d4dca21bbb4fe010afd6392ec8756c7b6
hbodan <hbodan@uamv.edu.ni>
2024-10-14 08:12:17 -0600

    Estructura lista

---
+++ sesion141024/numero2Grupal/EjercicioProductos/ValorTotal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioProductos
+{
+    
+
+
+    public static class Calcular
+    {
+
+        public static double calcularValorTotal()
+        {
+            //Aqui Rochu mete tu code
+        }
+    }
+}
