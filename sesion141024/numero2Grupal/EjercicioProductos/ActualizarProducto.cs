f4959dcae99caf90bbc9247a668f399036a7639e
Marie <fzogaib@uamv.edu.ni>
2024-10-14 08:35:06 -0600

    Actualizar Producto

--- sesion141024/numero2Grupal/EjercicioProductos/ActualizarProducto.cs
+++ sesion141024/numero2Grupal/EjercicioProductos/ActualizarProducto.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Crud;
+using Internal;
 
 namespace EjercicioProductos
 {
     public static class Actualizar
     {
 
-        public static double ActualizarProducto()
+
+        public void ActualizarStock(ref Producto[] inventario, string nombre, int nuevaCantidad)
         {
-            //Aqui Marie mete tu code
+            for (int i = 0; i < inventario.Length; i++)
+            {
+                if (inventario[i].Nombre == nombre)
+                {
+                    inventario[i].Cantidad = nuevaCantidad;
+                    //inventario[i] = new Producto(nombre, inventario[i].Precio, nuevaCantidad);
+                    Console.WriteLine($"Stock actualizado para {nombre}. Nueva cantidad: {nuevaCantidad}");
+                    return;
+                }
+            }
+            Console.WriteLine($"Producto {nombre} no encontrado en el inventario.");
         }
     }
 }
