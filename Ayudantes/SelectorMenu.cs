ffd1eb6eb5f3d38f76023fc950193f78a9aa80cc
acr301 <acrothschuh@uamv.edu.ni>
2024-10-21 10:50:27 -0600

    logica del menu interactivo implementada

--- Ayudantes/SelectorMenu.cs
+++ Ayudantes/SelectorMenu.cs
@@ -46,11 +46,75 @@ namespace Ayudantes
 
     public class SelectorMenu
     {
-        public int Menu(List<string> opciones)
-        {
-            int indiceInicial = 0;
 
-            return indiceInicial;
+        private List<Opcion> opciones;
+
+        public SelectorMenu(List<Opcion> opciones)
+        {
+            this.opciones = opciones;
+        }
+        public void MenuMostrar()
+        {
+
+            //inicializamos esto en 0 para que apunte al primer elemento
+            //de la lista opciones
+            int opcionSeleccionada = 0;
+
+            //instancia del enum ConsoleKey, contiene todas las teclas pulsables
+            ConsoleKeyInfo tecla;
+
+            do
+            {
+                Console.Clear();
+
+                //por cada una de las opciones
+                for (int i = 0; i < opciones.Count; i++)
+                {
+                    //se dara formato visual a la que este seleccionada
+                    if (i == opcionSeleccionada)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"> {opciones[i].Descripcion}");
+                        Console.ResetColor();
+                    }
+                    else //la que no este seleccionada
+                    {
+                        Console.WriteLine($"  {opciones[i].Descripcion}");
+                    }
+
+
+                    //overload booleano al metodo que pide leer, 
+                    //pasa esa tecla leida a tecla invocando el metodo .Key
+                    tecla = Console.ReadKey(true);
+
+                    switch (tecla.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            opcionSeleccionada--;
+
+                            // si se pulsa hacia arriba siendo el valor 0, es decir,
+                            // se vuelve -1, entonces le asignamos el ultimo valor,
+                            // para que "vaya hasta abajo"
+                            if (opcionSeleccionada < 0)
+                                opcionSeleccionada = opciones.Count - 1;
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            opcionSeleccionada++;
+
+                            // manda al principio
+                            if (opcionSeleccionada >= opciones.Count)
+                                opcionSeleccionada = 0;
+                            break;
+
+
+                        case ConsoleKey.Enter:
+                            // ejecutamos la accion de dicha opcion
+                            opciones[opcionSeleccionada].Accion.Ejecutar();
+                            break;
+                    }
+                }
+            } while (true);
         }
     }
 }
