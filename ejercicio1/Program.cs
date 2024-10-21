5040fec00fed0b814bb240d54e0692e66929dab2
acr301 <acrothschuh@uamv.edu.ni>
2024-10-21 11:11:58 -0600

    esqueleto de las acciones del menu del ejercicio 1, falta la logica que valide uniquidad de codigos, positivo de precios, y corra los metodos en la lista de acuerdo a las acciones, e instanciacion del menu con las Opciones y Acciones

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -1,9 +1,91 @@
 ﻿namespace ejercicio1;
+using Ayudantes;
+
+/* Crear un programa que simule la gestión de un inventario en una tienda. Utilizar un menú para
+agregar, eliminar, modificar y consultar productos en el inventario. Cada producto tendrá un
+código, nombre, cantidad y precio.
+Menú de opciones:
+1. Agregar producto
+2. Eliminar producto
+3. Modificar producto
+4. Consultar producto
+5. Mostrar todos los productos
+6. Salir
+*/
+
+
+public struct Producto
+{
+    public int Codigo;
+    public string Nombre;
+    public int Cantidad;
+    public double Precio;
+
+    public Producto(int codigo, string nombre, int cantidad, double precio) => (Codigo, Nombre, Cantidad, Precio) = (codigo, nombre, cantidad, precio);
+}
+
+public class Inventario
+{
+    public Producto Articulo;
+    public Inventario(Producto articulo) => (Articulo) = (articulo);
+}
+
+public class AgregarProducto : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+public class EliminarProducto : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+public class ModificarProducto : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+public class ConsultarProducto : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+public class MostrarTodos : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+public class Salir : IAccion
+{
+    public void Ejecutar()
+    {
+
+    }
+}
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+
+
     }
 }
